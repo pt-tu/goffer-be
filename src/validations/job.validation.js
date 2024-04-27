61996755377d2792b9ab799daa71010f7a510e5e
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-04-27 21:16:19 +0700

    Add api for list job and get job detail

--- src/validations/job.validation.js
+++ src/validations/job.validation.js
@@ -4,17 +4,17 @@ const { objectId } = require('./custom.validation');
 const createJob = {
   body: Joi.object().keys({
     title: Joi.string().required(),
-    description: Joi.string().required(),
+    description: Joi.string().required().min(200),
     location: Joi.string().required(),
     salaryFrom: Joi.string().required(),
     salaryTo: Joi.string(),
     experience: Joi.string().required(),
-    skills: Joi.array().items(Joi.string()).max(3).required(),
-    tools: Joi.array().items(Joi.string()).max(7).required(),
+    skills: Joi.array().items(Joi.string()).max(3).min(1).required(),
+    tools: Joi.array().items(Joi.string()).max(7).min(1).required(),
     slots: Joi.number().required(),
     time: Joi.string().required(),
     workingHours: Joi.number().required(),
-    orgId: Joi.string().required().custom(objectId),
+    org: Joi.string().required().custom(objectId),
   }),
 };
 
@@ -31,15 +31,23 @@ const getJobs = {
     slots: Joi.number(),
     time: Joi.string(),
     workingHours: Joi.number(),
-    orgId: Joi.string().custom(objectId),
+    org: Joi.string().custom(objectId),
     sortBy: Joi.string(),
     limit: Joi.number().integer(),
     page: Joi.number().integer(),
     populate: Joi.string(),
+    status: Joi.string().valid('unpublished', 'published', 'closed', 'expired'),
+  }),
+};
+
+const getJob = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(objectId),
   }),
 };
 
 module.exports = {
   createJob,
   getJobs,
+  getJob,
 };
