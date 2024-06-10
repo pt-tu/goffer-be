02b0d0001e328b69a2136d6952ed692dc7e2ebc3
Phan Thanh Tu <pttu2902@gmail.com>
2024-06-10 17:04:16 +0700

    remove jobs/individual api

--- src/routes/v1/job.route.js
+++ src/routes/v1/job.route.js
@@ -14,8 +14,6 @@ router
   .post(auth(), validate(jobValidation.createJob), jobController.createJob)
   .get(auth(), validate(jobValidation.getJobs), jobController.getJobs);
 
-router.route('/individual').get(validate(jobValidation.getJobs), jobController.getJobs);
-
 router.route('/:id').get(auth(true), validate(jobValidation.getJob), jobController.getJob);
 router.route('/:id/sourcing').get(auth(), jobController.getSourcing);
 router
