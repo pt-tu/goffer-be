6831e5d6d664c930b0fd9ceb8b019ba753c8a53b
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-06-13 22:38:39 +0700

    Update logic for genai service

--- src/validations/genai.validation.js
+++ src/validations/genai.validation.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const generateResponse = {
   body: Joi.object().keys({
     prompt: Joi.string().required().min(1),
+    maxTokens: Joi.number().optional(),
     systemMessage: Joi.string().optional(),
   }),
 };
