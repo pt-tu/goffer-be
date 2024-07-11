6294165462e7ef344b9b5871b7b59c2f8cf50086
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-07-11 21:13:03 +0700

    update limit

--- src/app.js
+++ src/app.js
@@ -33,7 +33,11 @@ app.use(cookies());
 app.use(helmet());
 
 // parse json request body
-app.use(express.json());
+app.use(
+  express.json({
+    limit: '50mb',
+  })
+);
 
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
