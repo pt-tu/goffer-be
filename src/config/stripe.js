13329dff137da3d305704027da89d254d9838f85
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-07-12 17:13:15 +0700

    update price and prod stripe

--- src/config/stripe.js
+++ src/config/stripe.js
@@ -1,11 +1,11 @@
 const PRICE_ENUM = {
-  ORG_LORD_PLAN: 'price_1PNFbOEJWFOdn6ijClFMYygE',
-  INDIVIDUAL_STAR_PLAN: 'price_1PSEIMEJWFOdn6ijQNzD3rcq',
+  ORG_LORD_PLAN: 'price_1PbgRNEJWFOdn6ijSuv4eYZF',
+  INDIVIDUAL_STAR_PLAN: 'price_1PbgRjEJWFOdn6ijQkOGFJMZ',
 };
 
 const PRODUCT_ENUM = {
-  ORG_LORD_PROD: 'prod_PpBL9oSOogZrFk',
-  INDIVIDUAL_STAR_PROD: 'prod_QIpy7hYrkdcIKu',
+  ORG_LORD_PROD: 'prod_QSbe25U09y7szv',
+  INDIVIDUAL_STAR_PROD: 'prod_QSbf1xKahXF62g',
 };
 
 module.exports = { PRICE_ENUM, PRODUCT_ENUM };
