f39a6a789ceb02ae3336c3ff45ca328bb410fcee
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-07-15 08:22:17 +0700

    disable recombee cascasde

--- src/utils/setupRecombee.js
+++ src/utils/setupRecombee.js
@@ -1,9 +1,9 @@
 const logger = require('../config/logger');
-const { addUserPropertiesToRecombee, addEntityPropertiesToRecombee } = require('../services/recombee.service');
+// const { addUserPropertiesToRecombee, addEntityPropertiesToRecombee } = require('../services/recombee.service');
 
 const setupRecombee = async () => {
   try {
-    await Promise.all([addUserPropertiesToRecombee(), addEntityPropertiesToRecombee()]);
+    // await Promise.all([addUserPropertiesToRecombee(), addEntityPropertiesToRecombee()]);
   } catch (error) {
     logger.info('Already added properties to Recombee');
   }
